fcdee617f1e65af6fdf3f68c18c9dcf6da6e0468
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-06-27 12:09:10 +0200

    Add support for integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -33,7 +33,4 @@ async def test_build_and_deploy(ops_test: OpsTest, indico_image, indico_nginx_im
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
     await ops_test.model.add_relation(APP_NAME, "redis-k8s")
     await ops_test.model.add_relation(APP_NAME, "postgresql-k8s:db")
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, "redis-k8s", "postgresql-k8s"], status="active"
-    )
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "waiting"
