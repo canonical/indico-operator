438ba73c0217d0eea0e6ceeda963bb4a4aa01c06
jdkandersson <david.andersson@canonical.com>
2022-08-05 14:39:57 +1000

    move building of charm and creating relations to fixture

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 import pytest
 import yaml
-from ops.model import ActiveStatus, WaitingStatus
+from ops.model import ActiveStatus
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
@@ -27,42 +27,31 @@ async def juju_run(unit, cmd):
     return stdout
 
 
+@pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest, indico_image, indico_nginx_image):
-    """Build the charm-under-test and deploy it together with related charms.
+async def test_active(ops_test: OpsTest, app_name: str, indico_charm):
+    """Check that the charm is active.
 
-    Assert on the unit status before any relations/configurations take place.
+    Assume that the charm has already been built and is running.
     """
     # build and deploy charm from local source folder
-    charm = await ops_test.build_charm(".")
-    resources = {
-        "indico-image": indico_image,
-        "indico-nginx-image": indico_nginx_image,
-    }
-    await ops_test.model.deploy("postgresql-k8s")
-    await ops_test.model.deploy("redis-k8s", "redis-broker")
-    await ops_test.model.deploy("redis-k8s", "redis-cache")
-    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
-    await ops_test.model.wait_for_idle()
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == WaitingStatus.name
-    await ops_test.model.add_relation(APP_NAME, "postgresql-k8s:db")
-    await ops_test.model.add_relation(APP_NAME, "redis-broker")
-    await ops_test.model.add_relation(APP_NAME, "redis-cache")
-    await ops_test.model.wait_for_idle(status="active")
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == ActiveStatus.name
+    assert ops_test.model.applications[app_name].units[0].workload_status == ActiveStatus.name
 
 
+@pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_health_checks(ops_test: OpsTest):
+async def test_health_checks(ops_test: OpsTest, indico_charm):
+    """Runs health checks for each container.
+
+    Assume that the charm has already been built and is running.
+    """
     container_list = ["indico", "indico-nginx", "indico-celery"]
     app = ops_test.model.applications["indico"]
     indico_unit = app.units[0]
     for container in container_list:
         result = await juju_run(
             indico_unit,
-            "PEBBLE_SOCKET=/charm/containers/{}/pebble.socket /charm/bin/pebble checks".format(
-                container
-            ),
+            f"PEBBLE_SOCKET=/charm/containers/{container}/pebble.socket /charm/bin/pebble checks",
         )
         # When executing the checks, `0/3` means there are 0 errors of 3.
         # Each check has it's own `0/3`, so we will count `n` times,
