692c0ea18979dad6e356dce8517a49bb5f003350
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-08-11 18:10:26 +0200

    Fix failing integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,79 +2,65 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-
-import logging
-from pathlib import Path
-
 import pytest
 import requests
-import yaml
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, Application
 from pytest_operator.plugin import OpsTest
-
-logger = logging.getLogger(__name__)
-
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = METADATA["name"]
-
 
 async def juju_run(unit, cmd):
     """Helper function that runs a juju command."""
-    result = await unit.run(cmd)
-    code = result.results["Code"]
-    stdout = result.results.get("Stdout")
-    stderr = result.results.get("Stderr")
+    action = await unit.run(cmd)
+    result = await action.wait()
+    code = result.results["return-code"]
+    stdout = result.results.get("stdout")
+    stderr = result.results.get("stderr")
     assert code == "0", f"{cmd} failed ({code}): {stderr or stdout}"
     return stdout
 
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_active(ops_test: OpsTest, app_name: str, indico_charm):
+async def test_active(ops_test: OpsTest, app: Application):
     """Check that the charm is active.
 
     Assume that the charm has already been built and is running.
     """
-    assert ops_test.model.applications[app_name].units[0].workload_status == ActiveStatus.name
+    assert app.units[0].workload_status == ActiveStatus.name
 
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_indico_is_up(ops_test: OpsTest, app_name: str, indico_charm):
+async def test_indico_is_up(ops_test: OpsTest, app: Application):
     """Check that the bootstrap page is reachable.
 
     Assume that the charm has already been built and is running.
     """
     # Read the IP address of indico
     status = await ops_test.model.get_status()
-    unit = list(status.applications[app_name].units)[0]
-    address = status["applications"][app_name]["units"][unit]["address"]
-
+    unit = list(status.applications[app.name].units)[0]
+    address = status["applications"][app.name]["units"][unit]["address"]
     # Send request to bootstrap page and set Host header to app_name (which the application
     # expects)
-    response = requests.get(f"http://{address}:8080/bootstrap", headers={"Host": app_name})
+    response = requests.get(f"http://{address}:8080/bootstrap", headers={"Host": app.name})
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
-@pytest.mark.abort_on_fail
-async def test_health_checks(ops_test: OpsTest, indico_charm):
+#@pytest.mark.abort_on_fail
+async def test_health_checks(ops_test: OpsTest, app: Application):
     """Runs health checks for each container.
 
     Assume that the charm has already been built and is running.
     """
+    unit = ops_test.model.applications[app.name].units[0]
     container_list = ["indico", "indico-nginx", "indico-celery"]
-    app = ops_test.model.applications["indico"]
-    indico_unit = app.units[0]
     for container in container_list:
         result = await juju_run(
-            indico_unit,
+            unit,
             f"PEBBLE_SOCKET=/charm/containers/{container}/pebble.socket /charm/bin/pebble checks",
         )
-        # When executing the checks, `0/3` means there are 0 errors of 3.
-        # Each check has it's own `0/3`, so we will count `n` times,
-        # where `n` is the number of checks for that container.
-        if container != "indico-nginx":
-            assert result.count("0/3") == 1
-        else:
-            assert result.count("0/3") == 2
+
+    if container != "indico-nginx":
+        assert result.count("0/3") == 1
+    else:
+        assert result.count("0/3") == 2
