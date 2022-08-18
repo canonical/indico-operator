5ed2313147885d9f5746c08cf8fc56af4c9bc920
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-08-18 11:38:16 +0200

    Add nginx prometheus exporter

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -23,8 +23,14 @@ def app_name(metadata):
     yield metadata["name"]
 
 
+@fixture(scope="module")
+def nginx_prometheus_exporter_image(metadata):
+    """Provides the nginx prometheus exporter image from the metadata."""
+    yield metadata["resources"]["nginx-prometheus-exporter-image"]
+
+
 @pytest_asyncio.fixture(scope="module")
-async def app(ops_test: OpsTest, app_name: str, pytestconfig: Config):
+async def app(ops_test: OpsTest, app_name: str, pytestconfig: Config, nginx_prometheus_exporter_image: str):
     """Indico charm used for integration testing.
 
     Builds the charm and deploys it and the relations it depends on.
@@ -41,9 +47,7 @@ async def app(ops_test: OpsTest, app_name: str, pytestconfig: Config):
     resources = {
         "indico-image": pytestconfig.getoption("--indico-image"),
         "indico-nginx-image": pytestconfig.getoption("--indico-nginx-image"),
-        "nginx-prometheus-exporter-image": metadata["resources"][
-            "nginx-prometheus-exporter-image"
-        ],
+        "nginx-prometheus-exporter-image": nginx_prometheus_exporter_image
     }
     application = await ops_test.model.deploy(
         charm, resources=resources, application_name=app_name
