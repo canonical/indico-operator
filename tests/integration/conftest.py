d874c9d47e9ceea7e510c71d9b2c5fab605acd6d
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-08-18 12:44:03 +0200

    Add nginx prometheus exporter

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -43,7 +43,8 @@ async def app(
         ops_test.model.deploy("redis-k8s", "redis-broker"),
         ops_test.model.deploy("redis-k8s", "redis-cache"),
     )
-    
+
+    charm = await ops_test.build_charm(".")
     resources = {
         "indico-image": pytestconfig.getoption("--indico-image"),
         "indico-nginx-image": pytestconfig.getoption("--indico-nginx-image"),
