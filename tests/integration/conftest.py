5e6c94eb7e2233b15e966d383bba402d06d8482d
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-08-18 12:36:11 +0200

    Add nginx prometheus exporter

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -26,7 +26,7 @@ def app_name(metadata):
 @fixture(scope="module")
 def nginx_prometheus_exporter_image(metadata):
     """Provides the nginx prometheus exporter image from the metadata."""
-    yield metadata["resources"]["nginx-prometheus-exporter-image"]
+    yield metadata["resources"]["nginx-prometheus-exporter-image"]["upstream-source"]
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -43,9 +43,7 @@ async def app(
         ops_test.model.deploy("redis-k8s", "redis-broker"),
         ops_test.model.deploy("redis-k8s", "redis-cache"),
     )
-
-    # Build and deploy the indico charm
-    charm = await ops_test.build_charm(".")
+    
     resources = {
         "indico-image": pytestconfig.getoption("--indico-image"),
         "indico-nginx-image": pytestconfig.getoption("--indico-nginx-image"),
