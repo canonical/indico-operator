74781d7b7ee04aa1a2054b229a35722149bed9b0
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-09-09 17:38:28 +0200

    Delete old lint, test and coverage comments when rerunning the pipeline

--- .github/files/comment_pr.js
+++ .github/files/comment_pr.js
@@ -3,22 +3,40 @@ module.exports = async ({github, context}) => {
     const report = JSON.parse(fs.readFileSync('./report.json'));
     const issue_number = report.number;
     const sha = report.sha;
+
     const createComment = async (body) => {
-    await github.issues.createComment({
+        await github.rest.issues.createComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issue_number,
+            body
+        });
+    }
+
+    const comments = await github.rest.issues.listComments({
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: issue_number,
-        body
     });
-    }
+    await createComment(comments)
+    // for (const comment of comments) {
+    //     if (comment.user.login == 'github-actions') {
+    //         await github.rest.issues.deleteComment({
+    //             owner: context.repo.owner,
+    //             repo: context.repo.repo,
+    //             comment_id: comment.id,
+    //         });
+    //     }
+    // }
+
     const coverageReport = report.reports.coverage.output.trim()
     if (!report.reports.lint.success) {
-    const lintReport = report.reports.lint.output.trim();
-    await createComment(`Lint checks failed for ${sha}\n\`\`\`\n${lintReport}\n\`\`\``);
+        const lintReport = report.reports.lint.output.trim();
+        await createComment(`Lint checks failed for ${sha}\n\`\`\`\n${lintReport}\n\`\`\``);
     }
     if (!report.reports.unit.success) {
-    const unitReport = report.reports.unit.output.trim();
-    await createComment(`Unit tests failed for ${sha}\n\`\`\`\n${unitReport}\n\`\`\``);
+        const unitReport = report.reports.unit.output.trim();
+        await createComment(`Unit tests failed for ${sha}\n\`\`\`\n${unitReport}\n\`\`\``);
     }
     await createComment(`Test coverage report for ${sha}\n\`\`\`\n${coverageReport}\n\`\`\``);
 }
