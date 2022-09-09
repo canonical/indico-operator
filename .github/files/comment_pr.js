074ce441b3d4459d63f69ecd303e531f963210a8
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-09-09 18:02:53 +0200

    Some stuff

--- .github/files/comment_pr.js
+++ .github/files/comment_pr.js
@@ -18,7 +18,7 @@ module.exports = async ({github, context}) => {
         repo: context.repo.repo,
         issue_number: issue_number,
     });
-    await createComment("RES" + JSON.stringify(comments))
+    await createComment("RES" + comments.data)
     // for (const comment of comments) {
     //     if (comment.user.login == 'github-actions') {
     //         await github.rest.issues.deleteComment({
