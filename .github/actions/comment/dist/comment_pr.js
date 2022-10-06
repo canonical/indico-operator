6799766d6b4cfd8e922e5454a8298ac6f4f63b53
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-10-06 11:24:36 +0200

    Extract comment logic to action

---
+++ .github/actions/comment/dist/comment_pr.js
@@ -0,0 +1,38 @@
+
+
+module.exports = async ({github, context}) => {
+    const core = require('@actions/core');
+    const github = require('@actions/github');
+    const fs = require('fs');
+    const reports = JSON.parse(fs.readFileSync(core.getInput('artifact-name')));
+    const issue_number = github.event.number;
+    console.log(`The pull request: ${github.event.pull_request}`);
+    console.log(`The event payload: ${github.event}`);
+
+    const deleteGithubActionsComments = async () => {
+        const comments = await github.rest.issues.listComments({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issue_number,
+        });
+        const githubActionsComments = comments.data.filter(comment => comment.user.login == 'github-actions[bot]')
+        for (const comment of githubActionsComments) {
+            await github.rest.issues.deleteComment({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                comment_id: issue_number,
+            });
+        }
+    }
+
+    await deleteGithubActionsComments();
+
+    for (const report of reports) {
+        await github.rest.issues.createComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issue_number,
+            r
+        });
+    }
+}
