ee86c43ddcb3dfc1a68c4ee7fcbda8a8eea7260e
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-10-06 17:36:46 +0200

    Extract comment logic to actionushush

--- .github/actions/comment_pr/dist/comment_pr.js
+++ .github/actions/comment_pr/dist/comment_pr.js
@@ -1,10 +1,8 @@
 
 
 module.exports = async ({github, context}) => {
-    const core = require('@actions/core');
     const fs = require('fs');
-    const artifactName = core.getInput('artifact-name');
-    const comments = JSON.parse(fs.readFileSync(`${artifactName}.json`));
+    const comments = JSON.parse(fs.readFileSync('${artifact-name}.json'));
     const issue_number = github.event.number;
     console.log(`The pull request: ${github.event.pull_request}`);
     console.log(`The event payload: ${github.event}`);
@@ -31,7 +29,7 @@ module.exports = async ({github, context}) => {
             await github.rest.issues.deleteComment({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
-                comment_id: issue_number,
+                comment_id: comment.id,
             });
         }
     }
