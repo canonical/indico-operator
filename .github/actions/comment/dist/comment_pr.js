d76e21b067f1a5dcdf6706b892f119d9b2c07be2
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-10-06 11:58:02 +0200

    Extract comment logic to action

--- .github/actions/comment/dist/comment_pr.js
+++ .github/actions/comment/dist/comment_pr.js
@@ -4,7 +4,7 @@ module.exports = async ({github, context}) => {
     const core = require('@actions/core');
     const github = require('@actions/github');
     const fs = require('fs');
-    const reports = JSON.parse(fs.readFileSync(core.getInput('artifact-name')));
+    const reports = JSON.parse(fs.readFileSync(`${core.getInput('artifact-name')}.json`));
     const issue_number = github.event.number;
     console.log(`The pull request: ${github.event.pull_request}`);
     console.log(`The event payload: ${github.event}`);
