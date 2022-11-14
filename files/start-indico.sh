5cac17a54719c2c0e79bed99085e4ba6ff3b1a18
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2022-11-14 12:59:48 +0100

    Force deployment on postgresql 12

--- files/start-indico.sh
+++ files/start-indico.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/usr/local/bin/indico db prepare
+/usr/local/bin/indico db prepare --force
 /usr/local/bin/indico db upgrade
 /usr/local/bin/indico db --all-plugins upgrade
 /usr/local/bin/uwsgi --ini /etc/uwsgi.ini
