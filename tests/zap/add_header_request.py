132e1baff6b1c2f04464be3815a6c5ec67ba0a57
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-01-12 16:24:07 -0300

    test(ISD-374): Fix lint

--- tests/zap/add_header_request.py
+++ tests/zap/add_header_request.py
@@ -1,8 +1,10 @@
-headers = dict({"Host": "indico.local"});
+headers = dict({"Host": "indico.local"})
 
-def sendingRequest(msg, initiator, helper):
+
+def sendingRequest(msg, initiator, helper):  # noqa: N802
     for h in list(headers):
-      msg.getRequestHeader().setHeader(h, headers[h]);
+        msg.getRequestHeader().setHeader(h, headers[h])
 
-def responseReceived(msg, initiator, helper):
-    pass;
+
+def responseReceived(msg, initiator, helper):  # noqa: N802
+    pass
--- tests/zap/hook.py
+++ tests/zap/hook.py
@@ -1,6 +1,14 @@
 def zap_started(zap, target):
-   print(zap.script.load('Add Header Script', 'httpsender', 'python : jython', '/zap/wrk/tests/zap/add_header_request.py'))
-   print(zap.script.enable('Add Header Script'))
+    print(
+        zap.script.load(
+            "Add Header Script",
+            "httpsender",
+            "python : jython",
+            "/zap/wrk/tests/zap/add_header_request.py",
+        )
+    )
+    print(zap.script.enable("Add Header Script"))
+
 
 def zap_pre_shutdown(zap):
     print("script.listEngines")
