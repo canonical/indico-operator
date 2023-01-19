177fdebfc3940ef900ac9fc4f38d215191f578c1
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-01-19 09:41:11 -0300

    tests(ISD-375): Add zap tests to verify how long it takes

---
+++ tests/zap/add_header_request.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-module-docstring,invalid-name,unused-argument
+def sendingRequest(msg, initiator, helper):  # noqa: N802
+    """sendingRequest is a name defined and expected by ZAP tool
+
+    Args:
+        msg (HttpMessage): all requests/responses sent/received by ZAP
+        initiator (int): the component that initiated the request
+        helper (HttpSender): returns the HttpSender instance used to send the request
+
+    Returns:
+        HttpMessage: returns HttpMessage updated
+    """
+    msg.setUserObject({"host": "indico.local"})
+    return msg
+
+
+def responseReceived(msg, initiator, helper):  # noqa: N802
+    """responseReceived is a name defined and expected by ZAP tool
+
+    Args:
+        msg (HttpMessage): all requests/responses sent/received by ZAP
+        initiator (int): the component that initiated the request
+        helper (HttpSender): returns the HttpSender instance used to send the request
+
+    Returns:
+        None
+    """
