0052d471383efbfd0c46675d6ce28bc968e4710b
Niels Robin-Aubertin <git@niels.fr>
2023-01-23 23:09:21 +0100

    Extend docstrings for helper functions

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -20,7 +20,18 @@ async def deploy_related_charm(
     raise_on_error: bool = True,
     trust: bool = False,
 ) -> Application:
-    """Deploy a charm related to our main app, if it's not already deployed."""
+    """Deploy a charm related to our main app, if it's not already deployed.
+
+    Args:
+        ops_test: The pytest operator plugin instance.
+        charm_name: The name of the charm to deploy.
+        instance_name: The name that the instance will have.
+        raise_on_error: Should an Exception be raised if the charm enters an error state.
+        trust: Should we trust the charm to change things on the k8s substrate.
+
+    Returns:
+        The deployed application.
+    """
     assert ops_test.model
     instance_name = instance_name or charm_name
     if instance_name not in ops_test.model.applications:
@@ -36,7 +47,17 @@ async def deploy_app(
     series: str,
     resources: Optional[dict] = None,
 ) -> Application:
-    """Deploy our main app, if it's not already deployed."""
+    """Deploy our main app, if it's not already deployed.
+
+    Args:
+        ops_test: The pytest operator plugin instance.
+        app_name: The name of the application's charm.
+        series: The Ubuntu series with which to deploy the charm.
+        resources: A dict with resources (like container images).
+
+    Returns:
+        The deployed application.
+    """
     assert ops_test.model
     if resources is None:
         resources = {}
@@ -55,7 +76,16 @@ async def relate_app(
     app_name: str,
     relation_id: str,
 ):
-    """Relate to applications if the relation does not already exists."""
+    """Relate to applications if the relation does not already exists.
+
+    Args:
+        ops_test: The pytest operator plugin instance.
+        app_name: The application name that will be part of the realtion.
+        relation_id: The relation identifier.
+
+    Returns:
+        The created relation.
+    """
     assert ops_test.model
     assert ops_test.model.relations
 
