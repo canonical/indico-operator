03c46a3c0edcb573298371a7658f2e7771fbb13e
Niels Robin-Aubertin <git@niels.fr>
2023-02-03 09:15:19 +0100

    Remove pylint disable by renaming var

--- plugins/autocreate/autocreate/plugin.py
+++ plugins/autocreate/autocreate/plugin.py
@@ -19,7 +19,5 @@ class AutocreatePlugin(IndicoPlugin):
         super().init()
         self.connect(signals.plugin.cli, self._extend_indico_cli)
 
-    # We need those arguments when extending indico's CLI
-    # pylint: disable=unused-argument
-    def _extend_indico_cli(self, *args, **kwargs):
+    def _extend_indico_cli(self, *_, **__):
         return cli
