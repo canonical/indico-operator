fc836243656d1e4338d6430263eb8b8ed699a031
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-03-17 16:46:20 -0300

    fix(ISD-260): Fix tox plugins

--- indico_rock/plugins/anonymize/anonymize/cli.py
+++ indico_rock/plugins/anonymize/anonymize/cli.py
@@ -4,39 +4,32 @@
 
 """Anonymize users non-interactively."""
 
-import click
 from hashlib import sha512
+
+import click
 from indico.cli.core import cli_group
 from indico.core.db import db
-from indico.modules.events.registration.models.registrations import Registration
 from indico.modules.events.registration.models.form_fields import RegistrationFormField
+from indico.modules.events.registration.models.registrations import Registration
 from indico.modules.users import User
-from indico.modules.users.util import search_users
 from indico.util.date_time import now_utc
-import re
 
-
-EMPTY_LIST = lambda _: list()
-EMPTY_SET = lambda _: set()
-EMPTY_VAL = lambda _: str
-HASH_VAL = lambda val: _hash(val)
 CLEAN_ATTRS = {
-    'affiliation' : EMPTY_VAL,
-    'email': EMPTY_VAL,
-    'secondary_emails': EMPTY_VAL,
-    'favorite_users': EMPTY_SET,
-    'favorite_categories': EMPTY_SET,
-    'identities': EMPTY_SET,
-    'old_api_keys': EMPTY_LIST,
-    'first_name': HASH_VAL,
-    'last_name': HASH_VAL,
-    'phone': HASH_VAL,
-    'address': HASH_VAL,
+    "affiliation": str(),
+    "email": str(),
+    "secondary_emails": str(),
+    "favorite_users": set(),
+    "favorite_categories": set(),
+    "identities": set(),
+    "old_api_keys": [],
 }
+HASH_ATTRS = ["first_name", "last_name", "phone", "address"]
+
 
 @cli_group(name="anonymize")
 def cli():
     """Anonymize users non-interactively."""
+
 
 # Extracted from:
 # https://github.com/bpedersen2/indico-cron-advanced-cleaner/
@@ -48,7 +41,8 @@ def _hash(val: str) -> str:
     Returns:
         First 12 characters from encoded data in hexadecimal format
     """
-    return sha512(val.encode('utf-8')+now_utc().isoformat().encode('utf-8')).hexdigest()[:12]
+    return sha512(val.encode("utf-8") + now_utc().isoformat().encode("utf-8")).hexdigest()[:12]
+
 
 # Based on:
 # https://github.com/bpedersen2/indico-cron-advanced-cleaner/
@@ -60,7 +54,11 @@ def anonymize_deleted_user(user: User):
     """
     for attr, anonymize_val in CLEAN_ATTRS.items():
         current_val = getattr(user, attr)
-        setattr(user, attr, anonymize_val(current_val))
+        setattr(user, attr, anonymize_val)
+    for attr in HASH_ATTRS:
+        current_val = getattr(user, attr)
+        setattr(user, attr, _hash(current_val))
+
 
 def anonymize_registration(registration: Registration):
     """Anonymize registration by changing specific attributes.
@@ -70,16 +68,16 @@ def anonymize_registration(registration: Registration):
     """
     for fid, rdata in registration.data_by_field.items():
         fieldtype = RegistrationFormField.get(oid=fid).input_type
-        if fieldtype in ('text', 'textarea'):
+        if fieldtype in ("text", "textarea"):
             rdata.data = _hash(rdata.data)
-        elif fieldtype == 'email':
-            rdata.data = _hash(rdata.data) + '@invalid.invalid'
-        elif fieldtype == 'phone':
-            rdata.data = '(+00) 0000000'
-        elif fieldtype == 'date':
+        elif fieldtype == "email":
+            rdata.data = _hash(rdata.data) + "@invalid.invalid"
+        elif fieldtype == "phone":
+            rdata.data = "(+00) 0000000"
+        elif fieldtype == "date":
             # Keep dates for now
             pass
-        elif fieldtype == 'country':
+        elif fieldtype == "country":
             # Keep country for now
             pass
         # other field types are not touched (choice, multiple choice, radio)
@@ -89,6 +87,7 @@ def anonymize_registration(registration: Registration):
     registration.email = _hash(registration.email)
     if registration.user:
         registration.user = None
+
 
 # Based on:
 # https://github.com/bpedersen2/indico-cron-advanced-cleaner/
@@ -103,6 +102,7 @@ def anonymize_registrations(user: User):
         anonymize_registration(registration)
     # db.session has commit()
     db.session.commit()  # pylint: disable=no-member
+
 
 @cli.command("user")
 @click.argument("email", type=str)
