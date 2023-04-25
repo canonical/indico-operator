b06a571dcea338a460195945142e79cb7835b88f
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2023-04-25 11:28:34 +0200

    Change load test threeshold

--- load_tests/load-test.js
+++ load_tests/load-test.js
@@ -9,7 +9,7 @@ export const options = {
   vus: 50,
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<1500'], // 95 percent of response times must be below 1500ms
+    http_req_duration: ['p(90)<1500'], // 90 percent of response times must be below 1500ms
   },
 };
 
