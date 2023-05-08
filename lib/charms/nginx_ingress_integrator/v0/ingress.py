3651a97c469247b77e9c77063b532450f55dc66f
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2023-05-08 10:25:35 +0200

    Test

--- lib/charms/nginx_ingress_integrator/v0/ingress.py
+++ lib/charms/nginx_ingress_integrator/v0/ingress.py
@@ -1,5 +1,5 @@
 # Copyright 2023 Canonical Ltd.
-# Licensed under the Apache2.0, see LICENCE file in charm source for details.
+# Licensed under the Apache2.0. See LICENSE file in charm source for details.
 """Library for the ingress relation.
 
 This library contains the Requires and Provides classes for handling
