3842f8e90b5cd8575c09a9aba47e7c942ffa3392
arturo-seijas <arturo.seijas@canonical.com>
2023-08-31 09:51:19 +0200

    Fix startup script

--- indico_rock/start-indico.sh
+++ indico_rock/start-indico.sh
@@ -3,8 +3,9 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-set -e
+indico db prepare
 
+set -e
 indico db upgrade
 indico db --all-plugins upgrade
 uwsgi --ini /etc/uwsgi.ini
