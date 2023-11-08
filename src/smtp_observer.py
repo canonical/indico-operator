cb8e47199629e150729f49d67bea7aa24f58bfb7
arturo-seijas <arturo.seijas@canonical.com>
2023-11-08 12:57:14 +0100

    Inline comments for implementation choices

--- src/smtp_observer.py
+++ src/smtp_observer.py
@@ -35,4 +35,5 @@ class SmtpObserver(Object):
 
     def _smtp_relation_data_available(self, _) -> None:
         """Handle the relation data available event."""
+        # A config changes is emited to avoid a huge refactor at this point.
         self._charm.on.config_changed.emit()
