622606feb1d716d41c8f12519f0b47c91e278148
arturo-seijas <arturo.seijas@canonical.com>
2024-01-04 10:41:32 +0100

    Update license headers date

--- lib/charms/smtp_integrator/v0/smtp.py
+++ lib/charms/smtp_integrator/v0/smtp.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # Licensed under the Apache2.0. See LICENSE file in charm source for details.
 
 """Library to manage the integration with the SMTP Integrator charm.
