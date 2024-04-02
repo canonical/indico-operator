09b00de0fd012c57d8bd799d630ce9ff816cabc4
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-04-02 14:11:01 +0200

    debug

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -7,6 +7,8 @@ curl https://pyenv.run | bash
 # ~/.bash_profile if it exists, otherwise ~/.profile (for login shells)
 # and ~/.bashrc (for interactive shells) :
 
+echo "Running as $(whoami), $HOME"
+
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
