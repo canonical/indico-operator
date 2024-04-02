1191d991b110afb0e744fc64966480b3d6bfab8e
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-04-03 00:08:22 +0200

    revert changes

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
-
+# Install python 3.12.2
+# We don't activate this since this is not required by default
 curl https://pyenv.run | bash
-
-# Load pyenv automatically by appending
-# the following to
-# ~/.bash_profile if it exists, otherwise ~/.profile (for login shells)
-# and ~/.bashrc (for interactive shells) :
-
-echo "Running as $(whoami), $HOME"
-
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-
 eval "$(pyenv virtualenv-init -)"
-
-cat <<- "EOF" >> "$HOME"/.profile
-        export PYENV_ROOT="$HOME/.pyenv"
-        [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
-EOF
-
-pyenv doctor
 pyenv install 3.12.2
-pyenv global 3.12.2
-python3 --version
+pyenv doctor
