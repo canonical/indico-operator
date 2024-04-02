b4d5a412d3ffa0f59f1e20ca1c07a821e49225cf
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-04-02 13:03:16 +0200

    pyenv pre-run-script

---
+++ tests/integration/pre_run_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl https://pyenv.run | bash
+
+# Load pyenv automatically by appending
+# the following to
+# ~/.bash_profile if it exists, otherwise ~/.profile (for login shells)
+# and ~/.bashrc (for interactive shells) :
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# Restart your shell for the changes to take effect.
+
+# Load pyenv-virtualenv automatically by adding
+# the following to ~/.bashrc:
+
+eval "$(pyenv virtualenv-init -)"
+
+cat <<- "EOF" >> "$HOME"/.bashrc
+        export PYENV_ROOT="$HOME/.pyenv"
+        [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+EOF
+
+pyenv install 3.12.2
+pyenv global 3.12.2
+python --version
