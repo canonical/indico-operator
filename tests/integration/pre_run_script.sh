c49e8d3e81fa56547829f1360e5ced1bb59fc97d
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-04-02 17:14:31 +0200

    update path, fix shell options and put values to ./profile

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -13,14 +13,9 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-# Restart your shell for the changes to take effect.
-
-# Load pyenv-virtualenv automatically by adding
-# the following to ~/.bashrc:
-
 eval "$(pyenv virtualenv-init -)"
 
-cat <<- "EOF" >> "$HOME"/.bashrc
+cat <<- "EOF" >> "$HOME"/.profile
         export PYENV_ROOT="$HOME/.pyenv"
         [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
