b5d79846dac181c96215c88fbbff64b37e749621
Arturo Seijas <arturo.seijas@canonical.com>
2024-04-05 15:50:21 +0200

    remove warning

--- tests/integration/test_actions.py
+++ tests/integration/test_actions.py
@@ -15,7 +15,6 @@ ADMIN_USER_EMAIL = "sample@email.com"
 ADMIN_USER_EMAIL_FAIL = "sample2@email.com"
 
 
-@pytest.mark.abort_on_fail
 @pytest_asyncio.fixture(scope="module")
 async def add_admin(app: Application):
     """
