e1d56aa1f232d5a530bfe5502c2b7bc702a8f202
Arturo Seijas <arturo.seijas@canonical.com>
2024-04-05 17:20:52 +0200

    fix integration tests

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -5,32 +5,44 @@
 """Indico S3 integration tests."""
 
 import json
+import re
+import yaml
 
 import juju
 import pytest
 from ops import Application
+from pytest_operator.plugin import OpsTest
 
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_s3(app: Application, s3_integrator: Application):
+@pytest.mark.usefixtures("s3_integrator")
+async def test_s3(app: Application, s3_integrator: Application, ops_test: OpsTest):
     """
     arrange: given charm integrated with S3.
     act: do nothing.
     assert: the pebble plan matches the S3 values as configured by the integrator.
     """
+
     # Application actually does have units
-    indico_container = app.units[0].get_container("indico")  # type: ignore
-    indico_env = indico_container.get_plan().services["indico"].environment
-    storage_config = json.loads(indico_env["STORAGE_DICT"])
+    return_code, stdout, _ = await ops_test.juju(
+        "ssh", "--container", app.name, app.units[0].name, "pebble", "plan"  # type: ignore
+    )
+    assert return_code == 0
+    plan = yaml.safe_load(stdout)
+    indico_env = plan["services"]["indico"]["environment"]
+    # STORAGE_DICT is a string representation of a Python dict
+    # pylint: disable=eval-used
+    storage_config = eval(indico_env["STORAGE_DICT"])  # nosec
     # Application actually does have units
     action: juju.action.Action = await s3_integrator.units[0].run_action(  # type: ignore
         "get-s3-connection-info"
     )
     await action.wait()
     assert action.status == "completed"
-    assert storage_config["s3"] == (
+    # in get-s3-connection-info, access_key and secret_key are redacted
+    assert re.match(
         f"s3:bucket={action.results['bucket']},host={action.results['endpoint']},"
-        f"access_key={action.results['access-key']},secret_key={action.results['secret-key']},"
-        "proxy=true"
+        f"access_key=[^=]+,secret_key=[^=]+,proxy=true",
+        storage_config["s3"],
     )
