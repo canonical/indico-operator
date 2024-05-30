d99741ab18d7dfb224e490d75d7c29737a34f246
Weii Wang <weii.wang@canonical.com>
2024-05-30 10:42:05 +0000

    Update workflow

--- indico_rock/start-indico.sh
+++ indico_rock/start-indico.sh
@@ -6,10 +6,10 @@
 set -e
 echo "Preparing DB"
 # We can ignore failures here, as this will fail if the DB isn't empty but isn't needed if it is
-indico db prepare || true
+indico db prepare &>> /srv/indico/log/indico.log || true
 echo "Upgrading DB"
-indico db upgrade
+indico db upgrade &>> /srv/indico/log/indico.log
 echo "Upgrading plugins"
-indico db --all-plugins upgrade
+indico db --all-plugins upgrade &>> /srv/indico/log/indico.log
 echo "Starting uwsgi"
 uwsgi --ini /etc/uwsgi.ini
