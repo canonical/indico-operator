fc6f46927b6ca8e49ed8b051668c1225ccf3430b
Weii Wang <weii.wang@canonical.com>
2024-05-31 06:19:44 +0000

    Update test_loki.py

--- tests/integration/test_loki.py
+++ tests/integration/test_loki.py
@@ -3,25 +3,31 @@
 # See LICENSE file for licensing details.
 
 """Indico Loki integration tests."""
-
+import asyncio
+import json
+import logging
 import time
 
 import pytest
 import requests
 from juju.application import Application
-from juju.unit import Unit
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_loki(loki: Application):
+async def test_loki(loki: Application, ops_test: OpsTest):
     """
     arrange: given charm integrated with Loki.
     act: do nothing.
     assert: Loki starts to receive log files from indico.
     """
-    loki_unit: Unit = loki.units[0]
-    loki_ip = await loki_unit.get_public_address()
+    _, status, _ = await ops_test.juju("status", "--format", "json")
+    status = json.loads(status)
+    loki_ip = next(status["applications"][loki.name]["units"].values())["address"]
+    logger.info("loki IP: %s", loki_ip)
     deadline = time.time() + 1200
     logged_files = []
     while time.time() < deadline:
@@ -35,7 +41,7 @@ async def test_loki(loki: Application):
                 .get("data", [])
             )
         except (requests.exceptions.RequestException, TimeoutError):
-            pass
+            await asyncio.sleep(1)
         if all(
             file in logged_files
             for file in ["/srv/indico/log/celery.log", "/srv/indico/log/indico.log"]
