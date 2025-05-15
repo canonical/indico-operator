8cb7ab7ebc1cf42241694c31fcb7ffdd546942c1
Ali Ugur <ali.ugur@canonical.com>
2025-05-15 16:03:16 +0300

    Chore: Passes standalone but not with tox :(

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,31 +1,31 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Fixtures for unit tests."""
-
-from secrets import token_hex
-
 import pytest
+from ops import pebble, testing
 
-import state
 
-
-@pytest.fixture(scope="function", name="proxy_config")
-def proxy_config_fixture(monkeypatch: pytest.MonkeyPatch):
-    """Proxy configuration with authentication and no_proxy values."""
-    # Mypy doesn't understand str is supposed to be converted to HttpUrl by Pydantic.
-    proxy_config = state.ProxyConfig(
-        http_proxy=f"http://testusername:{token_hex(16)}@httptest.internal:3127",  # type: ignore
-        https_proxy=f"http://testusername:{token_hex(16)}@httpstest.internal:3127",  # type: ignore
-        no_proxy="noproxy.host1,noproxy.host2",
-    )
-    monkeypatch.setattr(
-        state.os,
-        "environ",
+@pytest.fixture(scope="function", name="base_state")
+def base_state_fixture():
+    """State with container and config file set."""
+    pebble_layer = pebble.Layer(
         {
-            "JUJU_CHARM_HTTP_PROXY": str(proxy_config.http_proxy),
-            "JUJU_CHARM_HTTPS_PROXY": str(proxy_config.https_proxy),
-            "JUJU_CHARM_NO_PROXY": str(proxy_config.no_proxy),
-        },
+            "summary": "indico layer",
+            "description": "pebble config layer for indico",
+            "services": {
+                "indico": {},
+            },
+        }
     )
-    return proxy_config
+    yield {
+        "leader": True,
+        "containers": {
+            # mypy throws an error because it validates against ops.Container.
+            testing.Container(  # type: ignore[call-arg]
+                name="indico",
+                can_connect=True,
+                layers={"indico": pebble_layer},
+                service_statuses={"indico": pebble.ServiceStatus.ACTIVE},
+            )
+        },
+    }
---
+++ tests/unit/test_on_leader_elected.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from unittest.mock import MagicMock, patch
+
+import ops
+from ops import testing
+
+from charm import IndicoOperatorCharm
+
+
+@patch.object(ops.JujuVersion, "from_environ")
+def test_on_leader_elected_when_secrets_supported(mock_juju_env, base_state: dict):
+    """
+    arrange: charm created, leader selected and secrets supported
+    act: re-trigger the leader elected event
+    assert: the peer relation containers the secret-key
+    """
+    mock_juju_env.return_value = MagicMock(has_secrets=True)
+    base_state["relations"] = [testing.PeerRelation("indico-peers")]
+    state = testing.State(**base_state)
+    context = testing.Context(
+        charm_type=IndicoOperatorCharm,
+    )
+
+    context.run(context.on.leader_elected(), state)
+
+    print(state.get_relations("indico-peers"))
+    secret_id = state.get_relations("indico-peers")[0].local_app_data["secret-id"]
+    context.run(context.on.leader_elected(), state)
+
+    assert secret_id == state.get_relations("indico-peers")[0].local_app_data["secret-id"]
