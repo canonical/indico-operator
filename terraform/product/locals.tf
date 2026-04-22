600872097471d11811caabde6f6ce0f285236abf
Arturo Seijas <arturo.seijas@canonical.com>
2026-04-22 12:20:55 +0200

    Remove components that are not reused in the TF product

--- terraform/product/locals.tf
+++ terraform/product/locals.tf
@@ -7,8 +7,6 @@ locals {
     indico                   = "indico"
     redis_cache              = "redis-cache"
     redis_broker             = "redis-broker"
-    nginx_ingress_integrator = "nginx-ingress-integrator"
-    local_postgresql         = "postgresql-k8s"
   }
   app_names = merge(local.app_names_defaults, var.app_names)
 
@@ -26,22 +24,6 @@ locals {
 
   channels = merge(local.channels_defaults, var.channels)
 
-  config_lego = merge(var.config_lego, {
-    "plugin-config-secret-id" : (local.enable.nginx_ingress_integrator && local.enable.lego) ? juju_secret.lego_credentials[0].secret_id : null,
-  })
-
-  config_model = merge(
-    var.config_model,
-    {}
-  )
-
-  config_nginx_ingress_integrator = merge(
-    var.config_nginx_ingress_integrator,
-    {
-      service-hostname = var.hostname
-    }
-  )
-
   config_s3_integrator_media = var.config_s3_integrator_media
 
   config_smtp_integrator = merge(
@@ -54,50 +36,13 @@ locals {
     {}
   )
 
-  config_local_postgresql = merge(
-    var.config_local_postgresql,
-    {}
-  )
-
   config_indico = merge(var.config_indico, {})
 
   credentials = merge(
     var.credentials, {}
   )
 
-  enable_defaults = {
-    lego                     = false
-    local_saml_integrator    = false
-    local_postgresql         = false
-    nginx_ingress_integrator = false
-    redis_cache              = false
-    redis_broker             = false
-    s3_integrator_media      = false
-    smtp_integrator          = false
-  }
-
-  enable = merge(local.enable_defaults, var.enable)
-
-  integrate_offers_defaults = {
-    grafana         = false
-    postgresql      = true
-    prometheus      = false
-    saml_integrator = false
-  }
-
-  integrate_offers = merge(local.integrate_offers_defaults, var.integrate_offers)
-
-  model_defaults = {}
-
-  model = merge(local.model_defaults, var.model)
-
-  offer_urls_defaults = {}
-  offer_urls          = merge(local.offer_urls_defaults, var.offer_urls)
-
   revisions_defaults = {
-    lego = 61
-    # renovate: depName="nginx-ingress-integrator"
-    nginx_ingress_integrator = 145
     # renovate: depName="redis-k8s"
     redis_cache = 25
     # renovate: depName="redis-k8s"
@@ -110,8 +55,6 @@ locals {
     indico = 620
     # renovate: depName="saml-integrator"
     local_saml_integrator = 102
-    # renovate: depName="postgresql-k8s"
-    local_postgresql = 665
   }
 
   revisions = merge(local.revisions_defaults, var.revisions)
@@ -121,15 +64,4 @@ locals {
   }
 
   units = merge(local.units_defaults, var.units)
-
-  lego_secret = {
-    name = var.lego_secret.name
-    value = merge(
-      var.lego_secret.value,
-      {
-        httpreq-username = local.credentials.lego_httpreq_username
-        httpreq-password = local.credentials.lego_httpreq_password
-      }
-    )
-  }
 }
--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -1,6 +1,11 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+variable "juju_model_uuid" {
+  description = "Juju model UUID "
+  type        = string
+}
+
 variable "app_names" {
   description = "Partial overrides for application names."
   type        = map(string)
@@ -28,35 +33,6 @@ variable "channels" {
   default     = {}
 }
 
-variable "config_lego" {
-  description = "Configuration for the Lego charm."
-  type        = map(string)
-
-  default = {
-    "email" : "is-admin@canonical.com",
-    "plugin" : "httpreq",
-  }
-}
-
-variable "config_model" {
-  description = "Configuration for the juju model."
-  type        = map(string)
-  default = {
-    juju-http-proxy  = "" # override or set via locals
-    juju-https-proxy = "" # override or set via locals
-    juju-no-proxy    = "127.0.0.1,localhost,::1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,.canonical.com,.launchpad.net,.internal,.jujucharms.com,.ubuntu.com"
-  }
-}
-
-variable "config_nginx_ingress_integrator" {
-  description = "Configuration for the nginx ingress integrator charm."
-  type        = map(string)
-  default = {
-    max-body-size    = "21"
-    service-hostname = "" # override or set via locals
-  }
-}
-
 variable "config_s3_integrator_media" {
   description = "Configuration for the s3 integrator media charm."
   type        = map(string)
@@ -71,12 +47,6 @@ variable "config_s3_integrator_media" {
 
 variable "config_local_saml_integrator" {
   description = "Configuration for the local saml integrator charm."
-  type        = map(string)
-  default     = {}
-}
-
-variable "config_local_postgresql" {
-  description = "Configuration for the local postgresql charm."
   type        = map(string)
   default     = {}
 }
@@ -134,93 +104,10 @@ variable "credentials" {
   }
 }
 
-variable "enable" {
-  description = "A map to enable or disable various components."
-  type        = map(bool)
-  default     = {}
-  validation {
-    condition = length(
-      setsubtract(keys(var.enable), [
-        "lego",
-        "nginx_ingress_integrator",
-        "local_postgresql",
-        "local_saml_integrator",
-        "redis_cache",
-        "redis_broker",
-        "s3_integrator_media",
-        "smtp_integrator"
-      ])
-    ) == 0
-
-    error_message = "The keys in var.enable must be one or more of: lego, nginx_ingress_integrator, local_postgresql, local_saml_integrator, maubot, redis_k8s, s3_integrator_backup, s3_integrator_media, smtp_integrator."
-  }
-}
-
 variable "hostname" {
   description = "Matrix server URL."
   type        = string
   default     = ""
-}
-
-variable "integrate_offers" {
-  description = "Partial overrides for integrating specific offers."
-  type        = map(bool)
-  default     = {}
-  validation {
-    condition = length(
-      setsubtract(keys(var.integrate_offers), [
-        "postgresql",
-        "prometheus",
-        "grafana",
-        "saml_integrator"
-      ])
-    ) == 0
-
-    error_message = "The keys in var.integrate_offers must be one or more of: postgresql, prometheus, grafana, saml_integrator."
-  }
-}
-
-variable "lego_secret" {
-  description = "Lego secret config."
-  type = object({
-    name  = string
-    value = map(any)
-  })
-
-  default = {
-    name = "indico-lego-credentials"
-    value = {
-      httpreq-endpoint            = "https://lego-certs.canonical.com"
-      httpreq-username            = ""
-      httpreq-password            = ""
-      httpreq-propagation-timeout = 600
-    }
-  }
-}
-
-variable "model" {
-  description = "Partial overrides for the model configuration."
-  type        = map(string)
-  default     = {}
-}
-
-variable "offer_urls" {
-  description = "Partial overrides for external offer URLs."
-  type        = map(string)
-  default     = {}
-  validation {
-    condition = length(
-      setsubtract(keys(var.offer_urls), [
-        "postgresql",
-        "prometheus",
-        "grafana",
-        "saml_integrator"
-      ])
-    ) == 0
-
-    error_message = "The keys in var.offer_urls must be one or more of: postgresql, prometheus, grafana, saml_integrator."
-  }
-
 }
 
 variable "revisions" {
